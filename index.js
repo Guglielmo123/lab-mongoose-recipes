182a915bf9f9d1b7c35b0859e60bb9b55aa832c5
Guglielmo Galasso <guglielmo.galasso1@gmail.com>
2023-05-04 18:30:19 +0100

    recipes

--- index.js
+++ index.js
@@ -19,26 +19,42 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
-  } catch (error) {
+    let recipe1 = {title: 'Carbonara', level: 'Amateur Chef', cuisine: 'Italian'};
+    await Recipe.create(recipe1);
+    let findRecipe =  await Recipe.find({title: 'Carnbonara'});
+    console.log(findRecipe);
+
+    const insertedRecipes = await Recipe.insertMany(data);
+    console.log(`Inserted ${insertedRecipes.length} recipes:`);
+
+    // print title of each recipe 
+    insertedRecipes.forEach((recipe) => {
+      console.log(recipe.title);})
+
+    // lets update the Rigatoni alla Genovese as duration is wrong and should be 100 mins 
+    const updatedRigatoniDuration = await Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'},{$set: {duration: 100}}, {new: true});
+    /* The { new: true } option in the findOneAndUpdate() method tells 
+    Mongoose to return the updated document after the update operation is completed. 
+    By default, if the new option is not set to true, the method returns the document 
+    as it was before the update operation was performed. */
+
+    console.log(`Succesful update to: ${updatedRigatoniDuration}`)
+
+
+    // Remove a Recipe - Carrot Cake out of stock!
+
+    const removeCarrotCake = await Recipe.findByIdAndDelete('6453e96631c24a612674189a');
+    console.log(removeCarrotCake); // expected output is null since carrot cake object has been removed.
+
+    // close the connection with MongoDB database (free up resources and prevent memory leaks)
+      
+    await mongoose.connection.close();
+    console.log('Connection to database closed');
+
+  } catch(error) {
     console.log(error);
   }
 };
 
 manageRecipes();
 
-//Method 2: Using .then() method
-//If you want to use this method uncomment the code below:
-
-/* mongoose
-  .connect(MONGODB_URI)
-  .then((x) => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch((error) => {
-    console.error('Error connecting to the database', error);
-  }); */
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,6 +2,51 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
+  
+title: { // creating an object 
+        type: String,
+        required: true, // it is required 
+        unique: true, // must be required
+    },
+
+level: {
+        type: String,
+        enum: ['Easy Peasy', 'Amateur Chef','UltraPro Chef']
+
+},
+ingredients: {
+        type: [String],
+
+}, 
+cuisine: {
+        type: String,
+        required: true, 
+
+}, 
+dishType: {
+  type: String, 
+  enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+
+}, 
+image: {
+  type: String,
+  default: 'https://images.media-allrecipes.com/images/75131.jpg',
+  
+},
+duration: {
+  type: Number,
+  min: 0,
+
+},
+creator: {
+  type: String, 
+},
+created:{
+  type: Date,
+  default: Date.now,
+
+}
+  
   // TODO: write the schema
 });
 
